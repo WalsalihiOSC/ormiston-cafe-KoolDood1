15f3067e261e9f53af179f6761b3e7ebd75aa313
KoolDood1 <99300581+KoolDood1@users.noreply.github.com>
2022-06-21 09:23:00 +1200

    Add files via upload
    -Background color changed
    -Functional category buttons that change menu
    
    -Need to style category buttons
    -Add pictures and buttons(expand on) menu frames

--- Practice assessment.py
+++ Practice assessment.py
@@ -1,11 +1,8 @@
-
 #CSC Practice assessment app/coded design, Andre
+
 
 from tkinter import *
 
-#Data encapsulate later
-categories = ['Burger','With rice','Salads','Desserts','Drinks']
-burgers = ['Chicken Burger', 'Beef Burger', 'Butte Chicken Burger', 'Lamb Burger', 'Chilli burger', 'Legendary Burger']
 
 
 class interface:
@@ -28,6 +25,7 @@ class interface:
 
 class Menu:
     def __init__(self, parent):
+        self.d = Data() #set variable with button, pull that data and put in order list
         #Main 3 sections
         self.categoryframe = Frame(parent, highlightbackground="black", highlightthickness=1, bg="beige", height=700, width=333)
         self.categoryframe.grid(column=0, row=0)
@@ -41,32 +39,6 @@ class Menu:
         self.orderframe.grid(column=2, row=0)
         self.orderframe.pack_propagate(False)
 
-        # These are buttons created with a loop using the list of strings and placed with grid geometry
-        rcount = 0 #Starting Row count
-        for item in categories:
-            button = item
-            gridbuttons = Button(self.categoryframe, 
-                                 text=button)
-            gridbuttons.grid(sticky = W+E, row = rcount, padx=20)
-            rcount += 1
-            
-        
-        horizontal = 0 #Same as before, represents the starting placement position
-        vertical = 0
-        
-        for name in burgers: 
-            menuframes = Frame(self.mainframe, highlightbackground="black", highlightthickness=1)
-            menuframes.grid(column=horizontal, row=vertical, padx=10, pady=10)
-            #Add pictures into here
-            itemlabel = Label(menuframes, text= name)
-            itemlabel.pack()
-            horizontal += 1
-        
-            if horizontal == 3:
-                horizontal = 0
-                vertical = +1
-                vertical = vertical+1
-
         #Order confirmation widget
         displayorder = Frame(self.orderframe, highlightbackground="black", highlightthickness=1, height= 400, width=160)
         displayorder.grid_propagate(False)
@@ -74,6 +46,117 @@ class Menu:
         ordertext = Label(displayorder, text= "Order confrimation")
         ordertext.config(font=("Courier", 9))
         ordertext.grid(row=0, column=0,pady=10,padx=10)
+
+        # These are buttons created with a loop using the list of strings and placed with grid geometry
+        rcount = 0 #Starting Row count
+        for button in self.d.categories:
+            gridbuttons = Button(self.categoryframe, 
+                                 text=button, command=lambda button=button: self.menupick(button))
+            gridbuttons.grid(sticky = W+E, row = rcount, padx=20)
+            rcount += 1
+
+    def menupick(self, category):
+        self.d.selected = category
+        self.menugeneration()
+        
+
+    def menugeneration(self):
+        for child in self.mainframe.winfo_children():
+            child.destroy()
+
+        if self.d.selected == 'Burger':
+            horizontal = 0 #Same as before, represents the starting placement position
+            vertical = 0
+
+            items = self.d.burgers
+            for name in items: 
+
+                menuframes = Frame(self.mainframe, highlightbackground="black", highlightthickness=1)
+                menuframes.grid(column=horizontal, row=vertical, padx=10, pady=10)
+                    #Add pictures, button, price into here
+                itemlabel = Label(menuframes, text= name)
+                itemlabel.pack()
+                horizontal += 1
+                
+                if horizontal == 3:
+                    horizontal = 0
+                    vertical = +1
+                    vertical = vertical+1
+        elif self.d.selected == 'With rice':
+            horizontal = 0 
+            vertical = 0
+
+            items = self.d.withrice
+            for name in items: 
+
+                menuframes = Frame(self.mainframe, highlightbackground="black", highlightthickness=1)
+                menuframes.grid(column=horizontal, row=vertical, padx=10, pady=10)
+                    #Add pictures, button, price into here
+                itemlabel = Label(menuframes, text= name)
+                itemlabel.pack()
+                horizontal += 1
+                
+                if horizontal == 3:
+                    horizontal = 0
+                    vertical = +1
+                    vertical = vertical+1  
+        elif self.d.selected == 'Salads':
+            horizontal = 0 
+            vertical = 0
+
+            items = self.d.salads
+            for name in items: 
+
+                menuframes = Frame(self.mainframe, highlightbackground="black", highlightthickness=1)
+                menuframes.grid(column=horizontal, row=vertical, padx=10, pady=10)
+                    #Add pictures, button, price into here
+                itemlabel = Label(menuframes, text= name)
+                itemlabel.pack()
+                horizontal += 1
+                
+                if horizontal == 3:
+                    horizontal = 0
+                    vertical = +1
+                    vertical = vertical+1
+        elif self.d.selected == 'Desserts':
+            horizontal = 0 #
+            vertical = 0
+
+            items = self.d.desserts
+            for name in items: 
+
+                menuframes = Frame(self.mainframe, highlightbackground="black", highlightthickness=1)
+                menuframes.grid(column=horizontal, row=vertical, padx=10, pady=10)
+                    #Add pictures, button, price into here
+                itemlabel = Label(menuframes, text= name)
+                itemlabel.pack()
+                horizontal += 1
+                
+                if horizontal == 3:
+                    horizontal = 0
+                    vertical = +1
+                    vertical = vertical+1
+        elif self.d.selected == 'Drinks':
+            horizontal = 0 
+            vertical = 0
+
+            items = self.d.drinks
+            for name in items: 
+
+                menuframes = Frame(self.mainframe, highlightbackground="black", highlightthickness=1)
+                menuframes.grid(column=horizontal, row=vertical, padx=10, pady=10)
+                    #Add pictures, button, price into here
+                itemlabel = Label(menuframes, text= name)
+                itemlabel.pack()
+                horizontal += 1
+                
+                if horizontal == 3:
+                    horizontal = 0
+                    vertical = +1
+                    vertical = vertical+1
+        else:
+            pass
+    
   
 
 
@@ -82,13 +165,32 @@ class Menu:
 #Data class
 class Data:
     def __init__(self):
-        pass
+        self.selected = StringVar()
+        
+        self.categories = ['Burger','With rice','Salads','Desserts','Drinks']
+        
+        
+        self.burgers = ['Chicken Burger', 'Beef Burger', 'Fried Chicken Burger', 'Lamb Burger', 'Chilli burger', 'Legendary Burger']
+        self.burgerprice = [6.00, 6.50, 7.00, 7.00, 7.50, 10]
+
+        self.withrice = ['Butter Chicken', 'Teriyaki Chicken', 'Lamb curry', 'Potato curry', 'Chicken Katsu', 'Risotto'] 
+        self.withriceprice = [5.50, 6.00, 6.50, 5.00, 5.50, 6.00]
+
+        self.salads = ['Caesar salad', 'Greek salad', 'Italian salad', 'Egg salad', 'Fruit salad', 'Vegetable salad']
+        self.saladprice = [5.50, 5.00, 5.00, 4.50, 4.00, 4.50]
+
+        self.desserts = ['Caramel slice', 'Chocolate slice', 'Moosie', 'Ice pop', 'Brownie', 'Ice cream sandwich']
+        self.dessertprice = [3.50, 3.50, 2.00, 1.50, 3.50, 4.00]
+
+        self.drinks = ['Lipton ice tea', 'Up n go', 'Calci yum', 'Barista bros mocha', 'Anchor milk', 'Barista Bros espresso']
+        self.drinkprice = [4.50, 2.50, 2.00, 4.50, 2.00, 5.00]
+
      
     
 if __name__ =="__main__":
     root = Tk()
     gui = interface(root)
     root.geometry("1150x700")
-    root.resizable(False, False)#Maybe adjust resize
+    root.resizable(False, False)
     root.title("Ormiston cafe app")
     root.mainloop()
